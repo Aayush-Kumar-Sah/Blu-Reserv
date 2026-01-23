3e36d537041645887c703cc460bcd31fdfb11c0b
Aayush Kumar <150900263+Aayush-Kumar-Sah@users.noreply.github.com>
2026-01-24 03:05:46 +0530

    Change default route to LandingPage component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
     <Router>
       <Routes>
 
-        //<Route path="/" element={<MainApp />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/manager-login" element={<ManagerLogin />} />
         <Route path="/app" element={<MainApp />} />
 
