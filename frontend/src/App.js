3cfc01c3d2afe7e2e3df3e29bf15c8525a11d07d
Rushila V <rushilav@ibm.com>
2026-01-24 12:26:04 +0530

    modified notification preference

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ function App() {
     <Router>
       <Routes>
 
-        <Route path="/" element={<MainApp />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/manager-login" element={<ManagerLogin />} />
         <Route path="/app" element={<MainApp />} />
 
